9349350f52682333912c0e1415a29c7800ea7c06
Alec Bongard <alec.bongard@gmail.com>
2022-09-26 12:56:22 -0700

    Create computer choice function

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function getComputerChoice(){
-    
+    let choice = randomThree();
+    if (choice === 1){
+        return "Rock";
+    } else if(choice === 2){
+        return "Paper";
+    } else{
+        return "Scissors";
+    }
 }
 
 function randomThree(){ //Chooses one two or three randomly
-    return Math.floor(Math.random()*3) + 1
+    return Math.floor(Math.random()*3) + 1;
 }
