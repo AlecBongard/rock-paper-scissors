fc2328c0d0f6fcd54f4275fbf32737878db28782
Alec Bongard <alec.bongard@gmail.com>
2022-09-26 13:13:44 -0700

    Created playRound function

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 function getComputerChoice(){
     let choice = randomThree();
     if (choice === 1){
-        return "Rock";
+        return "rock";
     } else if(choice === 2){
-        return "Paper";
+        return "paper";
     } else{
-        return "Scissors";
+        return "scissors";
     }
 }
 
 function randomThree(){ //Chooses one two or three randomly
     return Math.floor(Math.random()*3) + 1;
+}
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection){
+        return "It's a draw!"
+    } else if((playerSelection === "rock" && computerSelection == "paper")
+    || (playerSelection === "scissors" && computerSelection === "rock")
+    || (playerSelection === "paper" && computerSelection === "scissors")){
+        return `You lose! ${computerSelection} beats ${playerSelection}!`;
+    } else{
+        return `You win! ${playerSelection} beats ${computerSelection}!`
+    }
 }
